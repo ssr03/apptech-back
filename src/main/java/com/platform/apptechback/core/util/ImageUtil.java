7e7ad4a76f0629f93afdf0b48005e30886be9929
keunyoungma <mky416@gmail.com>
2023-05-13 15:12:00 +0900

    메인화면에 로고 이미지 보이게

---
+++ src/main/java/com/platform/apptechback/core/util/ImageUtil.java
@@ -0,0 +1,54 @@
+package com.platform.apptechback.core.util;
+
+import com.platform.apptechback.domain.app.entity.App;
+import com.platform.apptechback.domain.app.repository.AppRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/image")
+public class ImageUtil {
+
+    private final AppRepository appRepository;
+
+    @GetMapping("/logo")
+    public ResponseEntity<Resource> getLogoImage(@RequestParam Long appId) {
+
+        App app = appRepository.findById(appId)
+                            .orElseThrow(IllegalArgumentException::new);
+
+        String appLogoFile = app.getAppLogoFile();
+
+        String imageRoot = "/Users/keunyoungma/IdeaProjects/apptechback/uploads/" + appLogoFile;
+
+        Resource resource = new FileSystemResource(imageRoot);
+
+        if(!resource.exists()){
+            return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+        }
+
+        HttpHeaders header = new HttpHeaders();
+        Path filePath = null;
+        try{
+            filePath = Paths.get(imageRoot);
+            header.add("Content-Type", Files.probeContentType(filePath));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<Resource>(resource, header, HttpStatus.OK);
+    }
+}
