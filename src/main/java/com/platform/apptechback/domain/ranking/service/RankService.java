523c4841b02e2ed61824dce4b356818c831d7678
ssr03 <ssr03@kidaristudio.com>
2024-01-06 13:18:55 +0900

    feature: ranking 리스트 api 추가 * API(GET /ranking )

--- src/main/java/com/platform/apptechback/domain/ranking/service/RankService.java
+++ src/main/java/com/platform/apptechback/domain/ranking/service/RankService.java
@@ -1,11 +1,22 @@
 package com.platform.apptechback.domain.ranking.service;
 
+import com.platform.apptechback.domain.ranking.entity.Rank;
+import com.platform.apptechback.domain.ranking.enums.RankType;
 import com.platform.apptechback.domain.ranking.repository.RankRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class RankService {
     private final RankRepository rankRepository;
+
+    public ResponseEntity<List<Rank>> getRanking(Long appId, RankType rankType){
+        List<Rank> rankingList =
+                rankRepository.findByAppIdAndType(appId, rankType);
+        return new ResponseEntity<>(rankingList, HttpStatus.OK);
+    }
 }
